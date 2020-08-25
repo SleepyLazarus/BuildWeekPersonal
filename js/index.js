0e5560d5de074ec7a697198682c012a06dc4f4c5
SleepyLazarus <68305546+SleepyLazarus@users.noreply.github.com>
2020-08-25 13:55:10 -0400

    Upload files

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// JS goes here
+
+function navMenu(){
+    let x = document.getElementById("myLinks");
+    if (x.style.display === "block"){
+        x.style.display = "none";
+    }else {
+        x.style.display = "block";
+    }
+}
